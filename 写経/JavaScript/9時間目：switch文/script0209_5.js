979f3a67c61e4feb25b636b5f71cc7817b7f5bbd
exyskns <freelancerelease@gmail.com>
2018-02-09 21:52:25 +0900

    02/09

---
+++ 写経/JavaScript/9時間目：switch文/script0209_5.js
@@ -0,0 +1,14 @@
+var age=70;
+
+switch(age){
+        case 70:
+        document.write("喜寿");
+        break;
+        
+        case 80:
+        document.write("米寿");
+        break;
+        
+    default:
+        document.write(age+"歳");
+}
