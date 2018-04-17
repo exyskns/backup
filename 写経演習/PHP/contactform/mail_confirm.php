d94879d7c594c5145aab75ea4d59ab391f94ad12
exyskns <freelancerelease@gmail.com>
2018-04-18 02:03:06 +0900

    04/17

--- 写経演習/PHP/contactform/mail_confirm.php
+++ 写経演習/PHP/contactform/mail_confirm.php
@@ -10,40 +10,34 @@
 <body>
 	<h1>お問い合わせ内容確認</h1>
     
-	<div class="box">
+	<div class="confirm">
 		<p>お問い合わせ内容はこちらで宜しいでしょうか？<br>
 		よろしければ、「送信する」ボタンを押して下さい。
 		</p>
 	
-	<div class="box1">
-		名前<br>
-	<?php echo $_POST['name'];?><br>
+		<P>名前<br>
+	<?php echo $_POST['name'];?></P>
 	
-	メールアドレス<br>
-	<?php echo $_POST['mail'];?><br>
+	<P>メールアドレス<br>
+        <?php echo $_POST['mail'];?></P>
 	
 	
-	年齢<br>
-	<?php echo $_POST['age'];?><br>
+	<P>年齢<br>
+	<?php echo $_POST['age'];?></P>
 	
 
-	コメント<br>
-	<?php echo $_POST['comments'];?><br>
-	
-	</div>
-	
+	<p>コメント<br>
+	<?php echo $_POST['comments'];?></p>
 	
 	<form action="index.html">
-		<input type="submit" class="submit1" value="戻って修正する"/>
-	</form>
+		<input type="submit" class="button1" value="戻って修正する"/></form>
 	
-	<form method="POST" action="insert.php" >
-	<input type="submit" class="submit2" value="登録する">
+	<form  action="insert.php" method="POST" >
+	<input type="submit" class="button2" value="登録する"/>
 	<input type="hidden" name="name" value="<?php echo $_POST['name'];?>">
 	<input type="hidden" name="mail" value="<?php echo $_POST['mail'];?>">
 	<input type="hidden" name="age" value="<?php echo $_POST['age'];?>">
-	<input type="hidden" name="comments" value="<?php echo $_POST['comments'];?>">
-	</form>
+	<input type="hidden" name="comments" value="<?php echo $_POST['comments'];?>"></form>
 	</div>
 	
 </body>
--- 写経演習/PHP/script.js
+++
@@ -1,2 +0,0 @@
-for(var a=18; a<=65; a++)
-    document.write(a)
