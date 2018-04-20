121ea34eb38a82a5b5e65e61146d01bf5e62c65b
exyskns <freelancerelease@gmail.com>
2018-04-20 21:54:44 +0900

    04/20

---
+++ 写経演習/PHP/4each_keijiban/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+
+<head>
+<meta charset="UTF-8">
+<title>4eachblog 掲示板</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+    
+<header>
+    <img src="4eachblog_logo.jpg">
+    <li>
+        <ul>トップ</ul>
+        <ul>プロフィール</ul>
+        <ul>4eachについて</ul>
+        <ul>登録フォーム</ul>
+        <ul>問い合わせ</ul>
+        <ul>その他</ul>
+    </li>
+</header>
+
+<body>
+    
+<div class="mainbox">
+    <div class="leftbox">
+        <h1>プログラミングに役立つ掲示板</h1>
+    </div>
+    
+    <div class="rightbox">
+        <p>人気の記事</p>
+            <li>
+                <ul>PHPオススメ本</ul>
+                <ul>PHP　MyAdminの使い方</ul>
+                <ul>いま人気のエディタTop5</ul>
+                <ul>HTMLの基礎</ul>
+            </li>
+        <p>オススメリンク</p>
+            <li>
+                <ul>インターノウス株式会社</ul>
+                <ul>XAMPPのダウンロード</ul>
+                <ul>Eclipseのダウンロード</ul>
+                <ul>Braketsのダウンロード</ul>
+            </li>
+        <p>カテゴリ</p>
+            <li>
+                <ul>HTML</ul>
+                <ul>PHP</ul>
+                <ul>MySQL</ul>
+                <ul>JavaScript</ul>
+            </li>
+            
+    </div>
+</div>
+    
+</body> 
+
+<footer>copyright internous | 4each blog is the one which provides A to Z about programming.
+</footer>
+    
+</html>
--- 写経演習/PHP/contactform/mail_confirm.php
+++ 写経演習/PHP/contactform/mail_confirm.php
@@ -11,33 +11,42 @@
 	<h1>お問い合わせ内容確認</h1>
     
 	<div class="confirm">
-		<p>お問い合わせ内容はこちらで宜しいでしょうか？<br>
-		よろしければ、「送信する」ボタンを押して下さい。
+		<p>お問い合わせ内容はこちらで宜しいでしょうか？
+            <br>よろしければ「送信する」ボタンを押して下さい。
 		</p>
 	
-		<P>名前<br>
-	<?php echo $_POST['name'];?></P>
+		<P>名前
+            <br>
+            <?php echo $_POST['name'];?>
+        </P>
 	
-	<P>メールアドレス<br>
-        <?php echo $_POST['mail'];?></P>
+	    <P>メールアドレス
+            <br>
+            <?php echo $_POST['mail'];?>
+        </P>
 	
-	
-	<P>年齢<br>
-	<?php echo $_POST['age'];?></P>
+	    <P>年齢
+            <br>
+            <?php echo $_POST['age'];?>
+        </P>
 	
 
-	<p>コメント<br>
-	<?php echo $_POST['comments'];?></p>
+        <p>コメント
+            <br>
+            <?php echo $_POST['comments'];?>
+        </p>
 	
-	<form action="index.html">
-		<input type="submit" class="button1" value="戻って修正する"/></form>
+        <form action="index.html">
+		  <input type="submit" class="button1" value="戻って修正する"/>
+        </form>
 	
-	<form  action="insert.php" method="POST" >
-	<input type="submit" class="button2" value="登録する"/>
-	<input type="hidden" name="name" value="<?php echo $_POST['name'];?>">
-	<input type="hidden" name="mail" value="<?php echo $_POST['mail'];?>">
-	<input type="hidden" name="age" value="<?php echo $_POST['age'];?>">
-	<input type="hidden" name="comments" value="<?php echo $_POST['comments'];?>"></form>
+	   <form action="insert.php" method="post">
+	       <input type="submit" class="button2" value="登録する"/>
+	       <input type="hidden" name="name" value="<?php echo $_POST['name'];?>">
+	       <input type="hidden" name="mail" value="<?php echo $_POST['mail'];?>">
+	       <input type="hidden" name="age" value="<?php echo $_POST['age'];?>">
+	       <input type="hidden" name="comments" value="<?php echo $_POST['comments'];?>">
+        </form>
 	</div>
 	
 </body>
