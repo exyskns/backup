4a3cfedbe34a62bf23ee2593ce00d1eaba5c46d4
exyskns <freelancerelease@gmail.com>
2018-05-25 22:03:10 +0900

    05/25

---
+++ 写経演習/Java/person/src/person/Preson.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package person;
+
+/**
+ * @author testuser
+ *
+ */
+public class Preson {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
