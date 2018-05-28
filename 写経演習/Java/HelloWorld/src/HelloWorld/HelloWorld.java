62c84a59a7f70cac5770159d0975d0d962aed312
exyskns <freelancerelease@gmail.com>
2018-05-28 22:04:24 +0900

    05/28

--- 写経演習/Java/HelloWorld/src/HelloWorld/HelloWorld.java
+++ 写経演習/Java/HelloWorld/src/HelloWorld/HelloWorld.java
@@ -19,7 +19,7 @@ public class HelloWorld {
 	public static void main(String[] args)throws IOException {
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("入力してください");
-		String t= br_readLine();
+		String t= br.readLine();
 		System.out.println(t);
 	}
 
