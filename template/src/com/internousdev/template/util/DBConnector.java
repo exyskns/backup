6679907c73a16c25157d1e7a2faa3833b5fa70b4
exyskns <freelancerelease@gmail.com>
2018-08-17 01:03:55 +0900

    08/16

--- template/src/com/internousdev/template/util/DBConnector.java
+++ template/src/com/internousdev/template/util/DBConnector.java
@@ -4,26 +4,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-		private static String driverName = "com.mysql.jdbc.Driver";
-		private static String url = "jdbc:mysql://localhost/ecsite";
+			private static String driverName = "com.mysql.jdbc.Driver";
+			private static String url = "jdbc:mysql://localhost/ecsite";
 
-		private static String user = "root";
-		private static String password = "mysql";
+			private static String user = "root";
+			private static String password = "mysql";
 
-		public Connection getConnection(){
+			public Connection getConnection() {
 			Connection con = null;
 
-		try{
+			try {
 					Class.forName(driverName);
-					con = (Connection)DriverManager.getConnection(url,user,password);
-		} catch(ClassNotFoundException e){
+					con = (Connection) DriverManager.getConnection(url,user,password);
+			} catch(ClassNotFoundException e) {
 				e.printStackTrace();
-
-		} catch(SQLException e){
+			} catch(SQLException e) {
 				e.printStackTrace();
-		}
-
-		return con;
-		}
-
+			}
+			return con;
+			}
 }
